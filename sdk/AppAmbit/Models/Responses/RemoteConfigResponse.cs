8b45b36edd4c54db4fc5fde22e446d29d9c006b0
Roel Leal <angel@kavaup.io>
2026-02-23 20:15:08 -0600

    chore: fixed response when is empty for remote config values

--- sdk/AppAmbit/Models/Responses/RemoteConfigResponse.cs
+++ sdk/AppAmbit/Models/Responses/RemoteConfigResponse.cs
@@ -1,4 +1,6 @@
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 
 namespace AppAmbit.Models.Responses;
@@ -6,5 +8,28 @@ namespace AppAmbit.Models.Responses;
 public class RemoteConfigResponse
 {
     [JsonProperty("configs")]
+    [JsonConverter(typeof(ConfigsDictionaryConverter))]
     public Dictionary<string, object> Configs { get; set; }
+}
+
+internal class ConfigsDictionaryConverter : JsonConverter<Dictionary<string, object>>
+{
+    public override Dictionary<string, object> ReadJson(JsonReader reader, Type objectType,
+        Dictionary<string, object> existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        var token = JToken.Load(reader);
+
+        if (token.Type == JTokenType.Object)
+        {
+            return token.ToObject<Dictionary<string, object>>(serializer);
+        }
+
+        // When API returns an empty array [] instead of an object {}, return empty dictionary
+        return new Dictionary<string, object>();
+    }
+
+    public override void WriteJson(JsonWriter writer, Dictionary<string, object> value, JsonSerializer serializer)
+    {
+        serializer.Serialize(writer, value);
+    }
 }
