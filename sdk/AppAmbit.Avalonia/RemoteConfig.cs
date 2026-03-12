f3cec43f1b830212f6bed2a6a8745a2cfe7ddab3
Jesus Marquez <jesus@kavaup.io>
2026-03-11 18:11:13 -0600

    feat: added support for Avalonia

--- sdk/AppAmbit.Avalonia/RemoteConfig.cs
+++ sdk/AppAmbit.Avalonia/RemoteConfig.cs
@@ -2,8 +2,33 @@ namespace AppAmbitAvalonia;
 
 public static class RemoteConfig
 {
-    public static void Enable()
+    public static bool Enable()
     {
-        AppAmbit.RemoteConfig.Enable();
+        return AppAmbit.RemoteConfig.Enable();
+    }
+
+    public static Task FetchAndStoreConfig()
+    {
+        return AppAmbit.RemoteConfig.FetchAndStoreConfig();
+    }
+
+    public static long GetLong(string key)
+    {
+        return AppAmbit.RemoteConfig.GetLong(key);
+    }
+
+    public static double GetDouble(string key)
+    {
+        return AppAmbit.RemoteConfig.GetDouble(key);
+    }
+
+    public static bool GetBoolean(string key)
+    {
+        return AppAmbit.RemoteConfig.GetBoolean(key);
+    }
+
+    public static string GetString(string key)
+    {
+        return AppAmbit.RemoteConfig.GetString(key);
     }
 }
